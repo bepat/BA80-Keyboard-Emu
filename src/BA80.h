752d44bd2e729f9d525d57f19b729b0dd9b08f2a
Benedikt Patt <git@benedikt-patt.de>
2022-04-28 20:35:16 +0200

    adding license information and some comments

--- src/BA80.h
+++ src/BA80.h
@@ -1,15 +1,38 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2022 Benedikt Patt
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef _BA80_H_
 #define _BA80_H_
 
 #include <Arduino.h>
 
 //settings for serial connection to VDU
-#define BA80_SERIAL_BAUD    4800
-#define BA80_SERIAL_CONF    SERIAL_8O1
-#define BA80_TX_BUFFER_SIZE 32
+#define BA80_SERIAL_BAUD                4800
+#define BA80_SERIAL_CONF                SERIAL_8O1
+#define BA80_TX_BUFFER_SIZE             32
 #define BA80_KEYBOARD_READY_INTERVAL    50
 
-
+//BA80 commands
 #define BA80_CMD_KEY_PRESSED    0xa0
 #define BA80_CMD_KEY_RELEASED   0xa8
 #define BA80_CMD_KEYBOARD_READY 0xf0
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2022 Benedikt Patt
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <Arduino.h>
 #include "config.h"
 #include "BA80.h"
@@ -9,6 +32,8 @@ PS2KeyAdvanced _ps2Keyboard;
 BA80 _ba80(&Serial);
 Bounce _powerButton = Bounce();
 
+//Array for mapping PS2 keys to B80 keys
+//This array is located in RAM, but can me moved to program memory, if the RAM capacity is too small on used AVR. 
 uint8_t _keyTranslation[] = {
     BA80_KEY_IGNORE,
     BA80_KEY_IGNORE, // PS2_KEY_NUM (0x01) 
